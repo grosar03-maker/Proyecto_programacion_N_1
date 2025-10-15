784fffbff49fa9c2813a0acead7f891054f98ced
Gustavo Rosa <grosar03@gmail.com>
2025-10-15 02:03:21 -0300

    Delete Proyecto(pedido, boleta y menu)/Ingrediente.py

--- Proyecto(pedido, boleta y menu)/Ingrediente.py
+++
@@ -1,9 +0,0 @@
-# Ingrediente.py
-class Ingrediente:
-    def __init__(self, nombre, unidad, cantidad):
-        self.nombre = nombre
-        self.unidad = unidad
-        self.cantidad = float(cantidad)
-
-    def __str__(self):
-        return f"{self.nombre} ({self.cantidad} {self.unidad})"
